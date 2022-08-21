e24dae110120630e66f20d7e056888795afbbb6b
Ao Luo <myloveyuxuan@gmail.com>
2022-08-22 03:47:41 +0800

    Update entrypoint.sh
    Signed-off-by: Ao Luo <myloveyuxuan@gmail.com>
    
    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
+while pg_isready -d gentooci -h db -U gentooci; [ $? -ne 0 ]; do
+  sleep 1
+done
+
 buildbot create-master -r /master
 mv /master/buildbot.tac /tinderbox-cluster
+rm -r /master
+
 buildbot checkconfig /tinderbox-cluster
-#FIXME GET CONFIG FROM ENVIRONMENT
+
+buildbot upgrade-master /tinderbox-cluster
+
+pushd /tinderbox-cluster
+rm master.cfg.sample
+popd
+
+pushd /tinderbox-cluster/buildbot_gentoo_ci/db/migrations
+PYTHONPATH+="/tinderbox-cluster" alembic ensure_version
+popd
+buildbot start --nodaemon /tinderbox-cluster || cat /tinderbox-cluster/twistd.log
